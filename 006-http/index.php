38893036f3523583fdacc5f49194076fbe0173ed
olesyasmk <dynamicasync@gmail.com>
2026-01-01 20:17:17 +0500

    Create index.php

---
+++ 006-http/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Список страниц</title>
+</head>
+<body>
+<h1>Список страниц для задания</h1>
+<ul>
+    <li><a href="page1.php">Страница 1 - Страница не найдена (404)</a></li>
+    <li><a href="page2.php?text=Привет, это тестовый файл!">Страница 2 - Скачать текстовый файл</a></li>
+    <li><a href="page3.php">Страница 3 - Страница с подсчётом открытий</a></li>
+    <li><a href="page4.php">Страница 4 - Результат подсчёта</a></li>
+</ul>
+</body>
+</html>
